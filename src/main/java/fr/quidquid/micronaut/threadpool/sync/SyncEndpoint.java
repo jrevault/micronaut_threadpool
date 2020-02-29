79ee505197615bd42a1df1d811c1d177d3f41f66
jrevault <jrevault@gmail.com>
2020-02-29 09:36:22 +0100

    Initial release

--- src/main/java/fr/quidquid/micronaut/threadpool/sync/SyncEndpoint.java
+++ src/main/java/fr/quidquid/micronaut/threadpool/sync/SyncEndpoint.java
@@ -2,11 +2,8 @@ package fr.quidquid.micronaut.threadpool.sync;
 
 import fr.quidquid.micronaut.threadpool.to.Book;
 import io.micronaut.http.*;
-import io.micronaut.http.annotation.Body;
-import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.*;
 import io.micronaut.http.annotation.Error;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Post;
 import io.micronaut.http.hateoas.JsonError;
 import io.micronaut.http.hateoas.Link;
 import org.slf4j.Logger;
@@ -26,14 +23,17 @@ public class SyncEndpoint {
     this.service = service;
   }
 
-  @Get( value = "/{id}", produces = MediaType.APPLICATION_JSON )
+  @Get("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
   public MutableHttpResponse<Book> get( long id) throws Exception {
       return HttpResponse.ok( service.get( id ) );
   }
 
 
-  @Post( value = "/", produces = MediaType.APPLICATION_JSON )
-  public MutableHttpResponse<Object> create( @Body String title) throws Exception {
+  @Post("/")
+  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+  @Produces(MediaType.APPLICATION_JSON)
+  public MutableHttpResponse<Object> create( String title) throws Exception {
     service.create( title );
     return HttpResponse.ok();
 
